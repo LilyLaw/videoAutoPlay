0ab6ac70e1bfccf19707a757236830497503cc30
LilyLaw <1444987087@qq.com>
2018-04-17 17:35:22 +0800

    视频自动播放

---
+++ resource.js
@@ -0,0 +1,24 @@
+let arr = [
+	{
+		src:"42.jpg"
+	},
+	{
+		src:"1.mp4"
+	},
+	{
+		src:"43.jpg"
+	},
+	{
+		src:"45.jpg"
+	},
+		
+	{
+		src:"2.mp4"
+	},
+	{
+		src:"v1.mp4"
+	},
+	{
+		src:"42.jpg"
+	}
+]
